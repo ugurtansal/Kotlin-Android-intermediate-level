b957f900c62d2abf5955c38be8380f3ee2099cdc
Uğur Tansal <ugur.tansal@gmail.com>
2025-05-15 09:05:07 +0300

    Applied data transfer via Intent

--- app/src/main/java/com/ugurtansal/kotlinandroidintermediate/lifecycle/SecondActivity.kt
+++ app/src/main/java/com/ugurtansal/kotlinandroidintermediate/lifecycle/SecondActivity.kt
@@ -8,12 +8,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ugurtansal.kotlinandroidintermediate.MainActivity
 import com.ugurtansal.kotlinandroidintermediate.R
+import com.ugurtansal.kotlinandroidintermediate.databinding.ActivityMainBinding
+import com.ugurtansal.kotlinandroidintermediate.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
         enableEdgeToEdge()
-        setContentView(R.layout.activity_second)
+        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -24,5 +29,8 @@ class SecondActivity : AppCompatActivity() {
 //            val intent = Intent(this, MainActivity::class.java)
 //            startActivity(intent)
 //        }
+
+        val data=intent.getStringExtra("name") //intent ile veri alma
+        binding.showName.text=data //veriyi textviewde gösterme
     }
 }
